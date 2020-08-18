2f2d30a4cce316c57646c2c21b49a3650de4c1b7
Florian Ott <florian.ott@gmx.at>
2020-08-18 15:36:34 +0100

    Rails task manager, all CRUD actions, not refactored, poor style.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #resources :restaurants
+
+  get 'tasks/new', to: 'tasks#new', as: 'newtask'
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edittask'
+  patch 'tasks/:id', to: 'tasks#update', as: 'updatetask'
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy'
 end
